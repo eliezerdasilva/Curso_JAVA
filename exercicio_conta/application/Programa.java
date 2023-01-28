dd53d946f40cbc9a69e297f9848ba8793aab2579
Eliezer <frete7132@gmail.com>
2023-01-28 11:47:32 -0300

    Upcasting e Downcasting

---
+++ exercicio_conta/application/Programa.java
@@ -0,0 +1,47 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Account acc = new Account(1000, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002,"Maria",0.0,500.0);
+	
+		// UPCASTING
+		
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "bob",0.0,3000.0);
+		Account acc3 = new SavingsAccount(1004, "Anaa",0.0, 0.0);
+		
+		//DOWNCATING 
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		
+		//Erro pos uma nao se extende da outra
+		//BusinessAccount acc5 = (BusinessAccount)acc3;
+		
+		//esse ira falhar
+		
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+			
+		}
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update");
+		}
+		
+		
+				
+		
+	}
+
+}
