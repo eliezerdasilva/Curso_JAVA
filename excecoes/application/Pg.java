7ce43b7ada0dafb27a89e0f2a7f58c4f843724b6
Eliezer <frete7132@gmail.com>
2023-01-29 20:42:09 -0300

    Resulução do execicio de reserva da forma menos correta

---
+++ excecoes/application/Pg.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Pg {
+	
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner ler = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Número do Quarto");
+		int number = ler.nextInt();
+		System.out.println("Data de check-in (dd/MM/yyyy) ");
+		Date checkIn = sdf.parse(ler.next());
+		System.out.println("Data de check-Out (dd/MM/yyyy) ");
+		Date checkOut = sdf.parse(ler.next());
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Data errada");
+		}
+		else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservado"+ reservation);
+			
+			System.out.println();
+			System.out.println("Entre com as data para reservar");
+			System.out.println("Data de check-in (dd/MM/yyyy) ");
+			checkIn = sdf.parse(ler.next());
+			System.out.println("Data de check-Out (dd/MM/yyyy) ");
+			checkOut = sdf.parse(ler.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			
+		}
+		
+		
+	}
+	
+
+}
