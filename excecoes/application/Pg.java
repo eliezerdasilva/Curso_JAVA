1f890fe53e38d3de9df51945b7de0fed32daa1c9
Eliezer <frete7132@gmail.com>
2023-02-08 21:07:03 -0300

    update better version of the excellence process

--- excecoes/application/Pg.java
+++ excecoes/application/Pg.java
@@ -6,40 +6,48 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Pg {
 	
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args){
 		
 		Scanner ler = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+	
+try {
 		System.out.println("Número do Quarto");
 		int number = ler.nextInt();
 		System.out.println("Data de check-in (dd/MM/yyyy) ");
 		Date checkIn = sdf.parse(ler.next());
 		System.out.println("Data de check-Out (dd/MM/yyyy) ");
 		Date checkOut = sdf.parse(ler.next());
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Data errada");
-		}
-		else {
-			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			System.out.println("Reservado"+ reservation);
-			
-			System.out.println();
-			System.out.println("Entre com as data para reservar");
-			System.out.println("Data de check-in (dd/MM/yyyy) ");
-			checkIn = sdf.parse(ler.next());
-			System.out.println("Data de check-Out (dd/MM/yyyy) ");
-			checkOut = sdf.parse(ler.next());
-			
-			reservation.updateDates(checkIn, checkOut);
-			
-		}
 		
 		
-	}
-	
+		Reservation reservation = new Reservation(number, checkIn, checkOut);
+		System.out.println("Reservado"+ reservation);
+		
+		System.out.println();
+		System.out.println("Entre com as data para reservar");
+		System.out.println("Data de check-in (dd/MM/yyyy) ");
+		checkIn = sdf.parse(ler.next());
+		System.out.println("Data de check-Out (dd/MM/yyyy) ");
+		checkOut = sdf.parse(ler.next());
+		
+		reservation.updateDates(checkIn, checkOut);
+		System.out.println("Reservado"+ reservation);
+		}
 
+	catch(ParseException e) {
+		System.out.println("Data informada incorreta.");
+		}
+	catch(DomainException	 e) {
+		System.out.println("Erro na reserva" + e.getMessage()); 
+	}
+	catch(RuntimeException e) {
+		System.out.println("Erro inesperado");
+	}
+	}
 }
+
+
--- excecoes/model/entities/Reservation.java
+++ excecoes/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 
@@ -14,7 +16,9 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
-		super();
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Erro na reserva");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -40,17 +44,17 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut)  {
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Erro : A data tem se ser posterior";
+			throw new DomainException("Erro : A data tem se ser posterior");
 		}
 		if(!checkOut.after(checkIn)) {
-			return "Erro na reserva";
+			throw new DomainException("Erro na reserva");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
+
 	}
 	@Override
 	public String toString() {
